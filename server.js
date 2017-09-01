7b85740613c676f3d791f729da0e50776b811d56
Daniel Oliveira <dnlsoliveira@gmail.com>
2017-09-01 09:45:24 -0400

    fix

--- server.js
+++ server.js
@@ -50,39 +50,3 @@ db.on("error", function(err) {
 db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
-
-
-var Sneaker1 = new Sneaker({
-  name: "NIKE AIR MAX 90 ULTRA 2.0 FLYKNIT",
-  img1: "https://images.nike.com/is/image/DotCom/PDP_HERO/875943_301_A_PREM/air-max-90-ultra-2-flyknit-mens-shoe.jpg",
-  img2: "https://images2.nike.com/is/image/DotCom/PDP_HERO/875943_301_B_PREM/air-max-90-ultra-2-flyknit-mens-shoe.jpg",
-  img3: "https://images3.nike.com/is/image/DotCom/PDP_HERO/875943_301_C_PREM/air-max-90-ultra-2-flyknit-mens-shoe.jpg",
-  img4: "https://images.nike.com/is/image/DotCom/PDP_HERO/875943_301_D_PREM/air-max-90-ultra-2-flyknit-mens-shoe.jpg"
-});
-
-var Sneaker2 = new Sneaker({
-  name: "NIKE METCON 3",
-  img1: "https://images.nike.com/is/image/DotCom/PDP_HERO/852928_009_A_PREM/metcon-3-mens-training-shoe.jpg",
-  img2: "https://images2.nike.com/is/image/DotCom/PDP_HERO/852928_009_B_PREM/metcon-3-mens-training-shoe.jpg",
-  img3: "https://images3.nike.com/is/image/DotCom/PDP_HERO/852928_009_C_PREM/metcon-3-mens-training-shoe.jpg",
-  img4: "https://images.nike.com/is/image/DotCom/PDP_HERO/852928_009_D_PREM/metcon-3-mens-training-shoe.jpg"
-});
-
-var Sneaker3 = new Sneaker({
-  name: "KOBE A.D. NXT",
-  img1: "https://images.nike.com/is/image/DotCom/PDP_HERO/882049_007_A_PREM/kobe-ad-nxt-mens-basketball-shoe.jpg",
-  img2: "https://images2.nike.com/is/image/DotCom/PDP_HERO/882049_007_B_PREM/kobe-ad-nxt-mens-basketball-shoe.jpg",
-  img3: "https://images3.nike.com/is/image/DotCom/PDP_HERO/882049_007_C_PREM/kobe-ad-nxt-mens-basketball-shoe.jpg",
-  img4: "https://images.nike.com/is/image/DotCom/PDP_HERO/882049_007_D_PREM/kobe-ad-nxt-mens-basketball-shoe.jpg"
-});
-
-var Sneaker4 = new Sneaker({
-  name: "CONVERSE CHUCK TAYLOR AMERICANA HIGH TOP",
-  img1: "https://images.nike.com/is/image/DotCom/PDP_HERO/M8437_602_A_PREM/converse-chuck-taylor-americana-high-top-unisex-shoe.jpg",
-  img2: "https://images2.nike.com/is/image/DotCom/PDP_HERO/M8437_602_B_PREM/converse-chuck-taylor-americana-high-top-unisex-shoe.jpg",
-  img3: "https://images3.nike.com/is/image/DotCom/PDP_HERO/M8437_602_C_PREM/converse-chuck-taylor-americana-high-top-unisex-shoe.jpg",
-  img4: "https://images.nike.com/is/image/DotCom/PDP_HERO/M8437_602_D_PREM/converse-chuck-taylor-americana-high-top-unisex-shoe.jpg"
-});
-
-var list = [Sneaker1, Sneaker2, Sneaker3, Sneaker4];
-Sneaker.insertMany(list);
